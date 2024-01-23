12793ea3d8769f572c89cabda7fae699dc859b2c
Sachini Sahasra <98446017+Sahasra0108@users.noreply.github.com>
2024-01-23 08:21:10 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,9 +1,10 @@
 function skillsMember() {
-    return {
-        restrict: 'E',
-        replace: true,
-        templateUrl: 'app/member/skillsMember.html',
-        controller: 'SkillsMemberCtrl',
-        controllerAs: 'skillsMember'
-    };
+  var self = this;
+  self.skills = ko.observableArray([]);
+  self.addSkill = function() {
+    self.skills.push(new Skill());
+  };
+  self.removeSkill = function(skill) {
+    self.skills.remove(skill);
+  };
 }
